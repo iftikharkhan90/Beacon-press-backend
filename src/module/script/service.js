71f8b314a554444e86790d453df72a48ed4a2065
Usama-Asghar445 <usamaasghar7426@gmail.com>
2025-10-09 05:20:54 -0700

    push

--- src/module/script/service.js
+++ src/module/script/service.js
@@ -3,20 +3,23 @@ const fs = require("fs");
 
 const saveFile = async (file) => {
   if (!file) return null;
+ //============================important===========
+  // const permpath = path.join(__dirname, "../../upload");
 
-  const permpath = path.join(__dirname, "../../upload");
+  // if (!fs.existsSync(permpath)) {
+  //   fs.mkdirSync(permpath, { recursive: true });
+  // }
+  //============================important===========
 
-  if (!fs.existsSync(permpath)) {
-    fs.mkdirSync(permpath, { recursive: true });
-  }
 
 
+  const permpath = "ali"// is ko delete kerna hy
   const ext = path.extname(file.name).toLowerCase();
   const base = path.basename(file.name, ext); 
   const newName = `${base}-${Date.now()}${ext}`;
   const filePath = path.join(permpath, newName);
 
-  await file.mv(filePath);
+  // await file.mv(filePath);
   return {
     name: file.name,
     url: ` http://localhost:4000/files/${newName}`, 
