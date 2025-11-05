6b7d5ca7e7d472edba7fd876b3403bd90003c49c
Usama-Asghar445 <usamaasghar7426@gmail.com>
2025-11-04 16:24:09 -0800

    push

--- src/module/user/controller.js
+++ src/module/user/controller.js
@@ -301,6 +301,28 @@ const userVerify = async (req, res) => {
   }
 };
 
+// =============================
+// Get User
+// =============================
+
+const getUser= async (req, res) => {
+  try {
+    const user = await User.find();
+    if (!user) {
+      return res.status(400).json({ success: false, messag: "user not found" });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "User fetched successfully",
+      data: user,
+    });
+  } catch (err) {
+    return res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+
+
 module.exports = {
   userCreate,
   userLogin,
@@ -309,4 +331,5 @@ module.exports = {
   updateUser,
   forgotPassword,
   resetPassword,
+  getUser
 };
--- src/module/user/index.js
+++ src/module/user/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { model } = require("mongoose");
 const  {verifyTokenAndAttachUser} = require("../../middleWare/validation/auth/index")
 const {validateUserRequest,loginvalidateUserRequest,UpdaUsertevalidateRequest} = require("../../middleWare/validation/user/index")
-const { userCreate, userLogin, userVerify, getUserById, updateUser, resetPassword, forgotPassword } = require("./controller");
+const { userCreate, userLogin, userVerify, getUserById, updateUser, resetPassword, forgotPassword ,getUser} = require("./controller");
 
 const router = express.Router();
 router.post("/", [validateUserRequest],userCreate);
+router.get("/getuser", [verifyTokenAndAttachUser],getUser);
 router.get("/get", [verifyTokenAndAttachUser],getUserById);
 router.patch("/patch",[verifyTokenAndAttachUser,UpdaUsertevalidateRequest],updateUser);
 router.post("/login",[loginvalidateUserRequest], userLogin);
